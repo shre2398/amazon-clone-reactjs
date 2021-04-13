763aba18e7a6f29e5b5054a151a682a73fe6ad47
Shreyas Chaudhari <shreya.chaudhari@sbdinc.com>
2021-04-13 10:57:29 +0530

    Created react app and cleaned initial code

---
+++ front-end/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAF1NSKN3UChNh1yS8stjwZ9p09PoRUZUA',
+  authDomain: 'todo-react-app-4f3fc.firebaseapp.com',
+  databaseURL: 'https://todo-react-app-4f3fc-default-rtdb.firebaseio.com',
+  projectId: 'todo-react-app-4f3fc',
+  storageBucket: 'todo-react-app-4f3fc.appspot.com',
+  messagingSenderId: '702142302877',
+  appId: '1:702142302877:web:02693246a14ec090aea274',
+  measurementId: 'G-TH83M92BT1'
+};
